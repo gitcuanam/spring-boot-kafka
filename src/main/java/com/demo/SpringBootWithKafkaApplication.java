fd4b7e4cb0a9395695271c38317611da9fdd21af
gitcuanam <nam.tranphuong.evotek@gmail.com>
2023-04-07 18:39:32 +0700

    fix build draft

--- src/main/java/com/demo/SpringBootWithKafkaApplication.java
+++ src/main/java/com/demo/SpringBootWithKafkaApplication.java
@@ -1,12 +1,27 @@
 package com.demo;
 
+import java.io.IOException;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.demo.engine.Producer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SpringBootWithKafkaApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWithKafkaApplication.class, args);
+		// SpringApplication.run(SpringBootWithKafkaApplication.class, args);
+		// Producer producer = new Producer();
+		// Consumer consumer = new Consumer();
+		// (new SpringBootWithKafkaApplication()).run();
+		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+		Producer fooService = ctx.getBean(Producer.class);		
+		fooService.sendMessage("Hello, Spring Kafka");
 	}
 }
--- src/main/java/com/demo/engine/Producer.java
+++ src/main/java/com/demo/engine/Producer.java
@@ -3,20 +3,23 @@ package com.demo.engine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Configurable
 public class Producer {
 
     private static final Logger logger = LoggerFactory.getLogger(Producer.class);
-    private static final String TOPIC = "users";
+    private static final String TOPIC = "test-topic";
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
         logger.info(String.format("#### -> Producing message -> %s", message));
+        System.out.println(this.kafkaTemplate);
         this.kafkaTemplate.send(TOPIC, message);
     }
 }
